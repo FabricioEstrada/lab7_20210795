e9839ce43d8620796f1d774f01ae9adfc6481c7b
FabricioEstrada <a20210795@pucp.edu.pe>
2024-10-10 19:26:42 -0500

    ola

---
+++ lab7-20210795/src/main/java/org/example/lab720210795/Lab720210795Application.java
@@ -0,0 +1,13 @@
+package org.example.lab720210795;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720210795Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720210795Application.class, args);
+    }
+
+}
---
+++ lab7-20210795/src/test/java/org/example/lab720210795/Lab720210795ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab720210795;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720210795ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
